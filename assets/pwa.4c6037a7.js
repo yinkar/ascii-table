a47a37fee815c907b037e93eba4f18ad01020ebd
Yinkar <hkrabs@yandex.com>
2022-02-22 19:43:35 +0300

    Updates

---
+++ assets/pwa.4c6037a7.js
@@ -0,0 +1,1 @@
+import{r}from"./index.5a3c9489.js";import"./vendor.b2e19c8a.js";r({});
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ascii-table/sw.js', { scope: '/ascii-table/' })})}

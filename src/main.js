a4edca7a2bcd5318dbe41cabc9f7234e936edbeb
Yinkar <hkrabs@yandex.com>
2022-02-22 19:44:15 +0300

    PWA support

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import App from './App.svelte';
 
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {}
+});
+
+if (typeof window !== 'undefined') {
+  import('./pwa');
+}
+
 const app = new App({
   target: document.getElementById('app')
 });
